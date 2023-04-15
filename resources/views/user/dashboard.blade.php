fd4e7e43cfd10f0c3da3f3776425ec5b9bb54596
Sebastian Selig <sebastian.selig@googlemail.com>
2023-04-15 12:57:15 +0200

    updated dark mode

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -18,7 +18,8 @@
                         Licensed entries for your account.
                     </h3>
                     <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
-                        <div class="overflow-hidden rounded-lg dark:bg-white/5 bg-gray-900 px-4 py-5 shadow sm:p-6">
+
+                        <div class="overflow-hidden rounded-lg dark:bg-white/5 bg-white px-4 py-5 shadow sm:p-6">
                             <dt class="truncate text-sm font-medium text-gray-500 dark:text-white">Jobs</dt>
                             <dd class="mt-1 text-3xl font-semibold tracking-tight text-gray-900 dark:text-gray-200">
                                 {{ $jobs }} / {{ $max_jobs }}
