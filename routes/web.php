d4ca3adf6581efa987b12706b32f2e30641b59fe
Sebastian Selig <sebastian.selig@googlemail.com>
2023-04-17 20:08:00 +0200

    removed unused routes for verification of repository

--- routes/web.php
+++ routes/web.php
@@ -30,8 +30,8 @@ Route::middleware([
 
     // Repository
     Route::resource(name: 'repository', controller: RepositoryController::class);
-    Route::get(uri: 'repository/{repository}/send-verification-token', action: [RepositoryController::class, 'send_verification'])->name(name: 'repository.send-verification-token');
-    Route::post(uri: 'repository/{repository}/verify-verification-token', action: [RepositoryController::class, 'verify_repository'])->name(name: 'repository.verify-verification-token');
+//    Route::get(uri: 'repository/{repository}/send-verification-token', action: [RepositoryController::class, 'send_verification'])->name(name: 'repository.send-verification-token');
+//    Route::post(uri: 'repository/{repository}/verify-verification-token', action: [RepositoryController::class, 'verify_repository'])->name(name: 'repository.verify-verification-token');
 
     // Endpoint
     Route::resource(name: 'endpoint', controller: EndpointController::class)->except(['show']);
@@ -50,16 +50,3 @@ Route::middleware([
     });
 
 });
-
-//Route::middleware(['auth:sanctum'])->prefix('api/v1')->name('api:')->group(function () {
-//
-//    // Test
-//    Route::get('test', function () {
-//        return response(['data' => ['message' => 'ok']], 200);
-//    })->name('test');
-//
-//    // GetEndpointJob
-//    Route::get(uri: 'jobs', action: \App\Http\Controllers\API\Client\GetEndpointJobController::class)->name(name: 'get-endpoint-jobs');
-//});
-
-Route::get(uri: 'endpointjob-wizard', action: \App\Http\Livewire\JobWizard::class)->name('endpointjob-wizard');
